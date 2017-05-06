3a9e479c529d68d9f6aface32548b50fe667f225
Blaine Rister <blaine@stanford.edu>
2017-05-06 00:48:04 -0700

    provide the option to give an image, to speed up aimReadCase

--- aimReadCase.m
+++ aimReadCase.m
@@ -1,9 +1,10 @@
-function [im, units, points] = aimReadCase(aimName, dcmDir)
+function [im, units, points] = aimReadCase(aimName, dcmDir, im)
 % Read an image and AIM annotation from files.
 %
 % Parameters:
 %   aimName - The name of the AIM file.
 %   dcmDir - The directory containing the DICOM files.
+%   im - Optionally provide the image. Otherwise it is read from disk.
 % 
 % Returns:
 %   im - See imRead3D.m.
@@ -14,10 +15,15 @@ function [im, units, points] = aimReadCase(aimName, dcmDir)
 dcmName = aimGetDicom(aimName, dcmDir);
 [slice, sliceUnits] = imRead3D(dcmName);
 
-% Read the full volume
-[im, units] = imRead3D(dcmDir);
-if ~isequal(sliceUnits, units)
-   error(['Slice ' dcmName ' and volume ' dcmDir ' have different units!'])
+if nargin < 3 || isempty(im)
+    % Read the full volume
+    [im, units] = imRead3D(dcmDir);
+    if ~isequal(sliceUnits, units)
+        error(['Slice ' dcmName ' and volume ' dcmDir ...
+            ' have different units!'])
+    end
+else
+    units = sliceUnits;
 end
 
 % Read the xy coordinates
