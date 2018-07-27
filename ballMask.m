a458728dcf2e882ceec55fc22ce944d1edd0fb96
Blaine Rister <blaine@stanford.edu>
2018-07-27 16:24:39 -0700

    add the ability to specify units

--- ballMask.m
+++ ballMask.m
@@ -1,15 +1,27 @@
-function mask = ballMask(siz, center, radius)
+function mask = ballMask(siz, center, radius, units)
 % Make an ball mask with the desired center and radius
+
+if nargin < 4 || isempty(units)
+    units = [1 1 1];
+end
+
+% Normalize the center, in mm
+nCenter = center .* units;
 
 dim = length(siz);
 switch dim
     case 2
     [X, Y] = meshgrid(1 : siz(2), 1 : siz(1));
-    mask = (X - center(2)) .^ 2 + (Y - center(1)) .^2 <= radius ^ 2; 
+    X = X * units(1);
+    Y = Y * units(2);
+    mask = (X - nCenter(2)) .^ 2 + (Y - nCenter(1)) .^ 2 <= radius ^ 2; 
     case 3
     [X, Y, Z] = ndgrid(1 : siz(1), 1 : siz(2), 1 : siz(3));
-    mask = (X - center(1)) .^ 2 + (Y - center(2)) .^2  + ...
-        (Z - center(3)) .^ 2 <= radius ^ 2; 
+    X = X * units(1);
+    Y = Y * units(2);
+    Z = Z * units(3);
+    mask = (X - nCenter(1)) .^ 2 + (Y - nCenter(2)) .^ 2  + ...
+        (Z - nCenter(3)) .^ 2 <= radius ^ 2; 
     otherwise
         error(['Unsupported dimensionality: ' num2str(dim)])
 end
